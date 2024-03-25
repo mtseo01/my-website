1fc37dc5eb5bbe5e4ba4dedf6ef6837a041b5857
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-25 23:54:43 +0900

    fix: disable experience component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Container from '@/app/_components/container';
 
 import Header from './_components/header';
+// import Experience from './_components/experience';
 
 export default function Index() {
   return (
     <main>
       <Container>
         <Header />
+        {/* <Experience /> */}
       </Container>
     </main>
   );
