79d95e7d9fd3557a40b8fd3c2709b31fe47ef75d
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-08 00:06:53 +0900

    add and delete image files, update image URLs in constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,16 @@
 export const EXAMPLE_PATH = 'blog-starter';
 export const CMS_NAME = 'Markdown';
+export const MTSEO_IMAGE_URL = '/assets/blog/author/mtseo.jpeg';
 export const HOME_OG_IMAGE_URL = '/assets/blog/images/og-image.jpg';
-// export const HOME_OG_IMAGE_URL2 = '/assets/blog/images/og-image.png';
+export const PLACEHOLDER_BLUR =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg==';
+
+export const GITHUB_ICON_URL = '/assets/blog/icons/github-mark.png';
+export const MAIL_ICON_URL = '/assets/blog/icons/mail-mark.png';
+
+// projects
+export const POMODORO_IMAGE_URL = '/assets/projects/images/pomodoro.png';
+export const AUTO_GIT_IMAGE_URL = '/assets/projects/images/auto-git.png';
+export const QQ_IMAGE_URL = '/assets/projects/images/qq.png';
+export const DAILY_IMAGE_URL = '/assets/projects/images/daily-record.png';
+export const MTSEO_DEV_IMAGE_URL = '/assets/projects/images/mtseo-dev.png';
