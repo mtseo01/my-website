f74cbe2228fddd48e7a84d786a4b6ba1419fa483
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-11 00:39:51 +0900

    style: add post title styling in PostPreview component

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -1,7 +1,5 @@
 import { type Author } from '@/interfaces/author';
 import Link from 'next/link';
-import Avatar from './avatar';
-import CoverImage from './cover-image';
 import DateFormatter from './date-formatter';
 
 type Props = {
@@ -13,34 +11,18 @@ type Props = {
   slug: string;
 };
 
-export function PostPreview({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: Props) {
+export function PostPreview({ title, date, excerpt, slug }: Props) {
   return (
-    <div className="my-6 animate-slideUp">
-      {/* <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div> */}
-      <h3 className="mb-3 text-2xl leading-snug md:text-3xl">
-        <Link
-          as={`/blog/${slug}`}
-          href="/blog/[slug]"
-          className="hover:underline"
-        >
+    <article className="my-7 animate-slideUp group">
+      <Link as={`/blog/${slug}`} href="/blog/[slug]">
+        <h3 className="mb-3 text-2xl leading-snug text-transparent bg-black bg-clip-text group-hover:bg-gradient-to-r group-hover:from-blue-800 group-hover:to-sky-400 sm:text-3xl">
           {title}
-        </Link>
-      </h3>
-
-      <p className="mb-3 text-lg leading-relaxed">{excerpt}</p>
-      <div className="text-lg text-gray-500 ">
-        <DateFormatter dateString={date} />
-      </div>
-      {/* <Avatar name={author.name} picture={author.picture} /> */}
-    </div>
+        </h3>
+        <p className="mb-3 text-lg leading-relaxed">{excerpt}</p>
+        <div className="text-lg text-gray-500 ">
+          <DateFormatter dateString={date} />
+        </div>
+      </Link>
+    </article>
   );
 }
