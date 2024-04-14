3c41224a16a2ba4d15655b8cc9ec061e58d5bb0a
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-14 10:56:44 +0900

    refactor: post-preview component styles

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -13,13 +13,13 @@ type Props = {
 
 export function PostPreview({ title, date, excerpt, slug }: Props) {
   return (
-    <article className="my-7 animate-slideUp group">
+    <article className="my-7  group">
       <Link as={`/blog/${slug}`} href="/blog/[slug]">
-        <h3 className="mb-3 text-2xl leading-snug text-transparent bg-black bg-clip-text group-hover:bg-gradient-to-r group-hover:from-blue-800 group-hover:to-sky-400 sm:text-3xl">
+        <h3 className="mb-3 text-xl leading-snug text-transparent bg-black bg-clip-text group-hover:bg-gradient-to-r group-hover:from-blue-800 group-hover:to-sky-400 sm:text-2xl">
           {title}
         </h3>
-        <p className="mb-3 text-lg leading-relaxed">{excerpt}</p>
-        <div className="text-lg text-gray-500 ">
+        <p className="mb-3 text-base sm:text-lg leading-relaxed">{excerpt}</p>
+        <div className="text-base sm:text-lg text-gray-500 ">
           <DateFormatter dateString={date} />
         </div>
       </Link>
