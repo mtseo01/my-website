51601c4e658d5e8a611d5eb4674bbec0e7b4ce50
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-04-15 19:27:33 +0900

    add ryhypeRaw to markdownToHtml

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -6,15 +6,17 @@ import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeRaw from 'rehype-raw'; // rehypeRaw 추가
 
 export default async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse) // 마크다운 파싱
     .use(remarkGfm) // GitHub Flavored Markdown 활성화
-    .use(remarkRehype) // 마크다운을 HTML로 변환
+    .use(remarkRehype, { allowDangerousHtml: true }) // 마크다운을 HTML로 변환
     .use(rehypePrettyCode, { theme: 'github-dark' })
     .use(rehypeSlug) // 제목에 ID 슬러그 생성
     .use(rehypeAutolinkHeadings, { behavior: 'prepend' }) // 제목에 자동 링크 추가
+    .use(rehypeRaw)
     .use(rehypeStringify) // HTML 문자열로 변환
     .process(markdown);
   return result.toString();
