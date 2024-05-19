0305cb5231737452eb3c526f77c96aa63e76b472
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-05-19 23:56:07 +0900

    add post and note lists to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Container from '@/app/_components/container';
 import Header from './_components/header';
 import SelfIntro from './_components/self-intro';
+import PostListForMain from './_components/post-list-for-main';
+import NoteListForMain from './_components/note-list-for-main';
 
 export default function Index() {
   return (
@@ -9,6 +11,12 @@ export default function Index() {
         <Header />
         <section className="max-w-2xl mx-auto my-6">
           <SelfIntro />
+          <hr className="my-5" />
+          <div className="flex flex-col">
+            <PostListForMain />
+            <hr className="my-5" />
+            <NoteListForMain />
+          </div>
         </section>
       </Container>
     </main>
