dfed7510500ba8b33f55099c8d563027a8764388
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-05-31 23:25:10 +0900

    feat: add tag filtering functionality to NoteList component

--- src/app/_components/note-list.tsx
+++ src/app/_components/note-list.tsx
@@ -1,15 +1,30 @@
+'use client';
 import { NotePreview } from './note-preview';
 import { Note } from '@/interfaces/note';
+import { useSearchParams } from 'next/navigation';
 
 type Props = {
   notes: Note[];
 };
 
 export function NoteList({ notes }: Props) {
+  const currentTags = useSearchParams().getAll('tag');
+
+  const filteredNotes = notes.filter((note) =>
+    note.tags.some((tag) => currentTags.includes(tag))
+  );
+
+  const currentNotes = filteredNotes.length > 0 ? filteredNotes : notes;
+
   return (
     <section className="max-w-2xl mx-auto">
+      <div className="my-6">
+        <span>총 </span>
+        <strong>{currentNotes.length}</strong>
+        <span>개의 노트</span>
+      </div>
       <div className="flex-col my-6">
-        {notes.map((note) => (
+        {currentNotes.map((note) => (
           <NotePreview
             key={note.slug}
             title={note.title}
