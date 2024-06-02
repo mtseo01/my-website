b683ead8c9f6482c3bfca6f005a5c7cb29ca0830
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-06-02 22:32:38 +0900

    refactor: styles in PostPreview and PosTtitle components

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -13,12 +13,12 @@ type Props = {
 
 export function PostPreview({ title, date, slug }: Props) {
   return (
-    <article className="my-7  group">
+    <article className="my-7 group">
       <Link as={`/blog/${slug}`} href="/blog/[slug]">
         <h3 className="mb-3 text-lg leading-snug text-transparent bg-black bg-clip-text group-hover:bg-gradient-to-r group-hover:from-blue-800 group-hover:to-sky-400 sm:text-xl">
           {title}
         </h3>
-        <div className="text-sm sm:text-base text-gray-500 ">
+        <div className="text-sm text-gray-500 sm:text-base ">
           <DateFormatter dateString={date} />
         </div>
       </Link>
--- src/app/_components/post-title.tsx
+++ src/app/_components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostTitle({ children }: Props) {
   return (
-    <h1 className="max-w-2xl mx-auto mb-12 text-5xl font-bold leading-tight tracking-tighter text-center md:leading-none md:text-left">
+    <h1 className="max-w-2xl mx-auto mb-6 text-2xl font-bold tracking-tighter l md:text-5xl eading-tight sm:text-3xl md:leading-none md:text-left">
       {children}
     </h1>
   );
