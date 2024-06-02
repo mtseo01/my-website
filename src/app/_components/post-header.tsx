f86ad61298243b44aab4ba90ae9c02c1a095e395
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-06-02 22:31:15 +0900

    feat: add tags to PostHeader

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -1,31 +1,33 @@
-import Avatar from './avatar';
-import CoverImage from './cover-image';
+import Link from 'next/link';
 import DateFormatter from './date-formatter';
 import { PostTitle } from '@/app/_components/post-title';
-import { type Author } from '@/interfaces/author';
 
 type Props = {
   title: string;
-  coverImage: string;
   date: string;
-  author: Author;
+  tags?: string[];
 };
 
-export function PostHeader({ title, coverImage, date, author }: Props) {
+export function PostHeader({ title, date, tags }: Props) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        {/* <Avatar name={author.name} picture={author.picture} /> */}
-      </div>
-      {/* <div className=" mb-8 md:mb-16 sm:mx-0">
-        <CoverImage title={title} src={coverImage} />
-      </div> */}
+
       <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          {/* <Avatar name={author.name} picture={author.picture} /> */}
-        </div>
-        <div className="mb-6 text-lg">
+        {tags?.length ? (
+          <div className="flex flex-wrap mb-6">
+            {tags.map((tag, i) => (
+              <Link
+                className="mb-1 mr-2 text-blue-600 text-md sm:text-lg hover:underline"
+                href={`/notes?tag=${tag}`}
+                key={i}
+              >
+                #{tag}
+              </Link>
+            ))}
+          </div>
+        ) : null}
+        <div className="mb-6 text-md sm:text-lg">
           <DateFormatter dateString={date} />
         </div>
       </div>
