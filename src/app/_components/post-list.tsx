772307033d382d7f303c8b70705f572923ce9546
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-08-27 23:16:56 +0900

    feat: add tag filtering functionality to PostList component

--- src/app/_components/post-list.tsx
+++ src/app/_components/post-list.tsx
@@ -1,15 +1,30 @@
+'use client';
 import { Post } from '@/interfaces/post';
 import { PostPreview } from './post-preview';
+import { useSearchParams } from 'next/navigation';
 
 type Props = {
   posts: Post[];
 };
 
 export function PostList({ posts }: Props) {
+  const currentTags = useSearchParams().getAll('tag');
+
+  const filteredPosts = posts.filter((post) =>
+    post.tags.some((tag) => currentTags.includes(tag))
+  );
+
+  const currentPosts = filteredPosts.length > 0 ? filteredPosts : posts;
+
   return (
     <section className="max-w-2xl mx-auto">
+      <div className="my-6">
+        <span>총 </span>
+        <strong>{currentPosts.length}</strong>
+        <span>개의 노트</span>
+      </div>
       <div className="flex-col my-6">
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <PostPreview
             key={post.slug}
             title={post.title}
